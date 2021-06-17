6e30baf2af563478a416ee6e8f02c6e663dc5bee
Ansoriddin <ansoriddininternet@gmail.com>
2021-06-17 12:18:14 +0500

    my first telegram bot

---
+++ inline_keyboards.js
@@ -0,0 +1,154 @@
+module.exports = {
+	inline_keyboard_1: [
+		[
+			{
+				text: 'RUB - ₽',
+				callback_data: 'RUB'
+			},
+			{
+				text: 'SEK - Skr',
+				callback_data: 'SEK'
+			},
+			{
+				text: 'SGD - S$',
+				callback_data: 'SGD'
+			}
+		],
+		[
+			{
+				text: 'TRY - TL',
+				callback_data: 'TRY'
+			},
+			{
+				text: 'UAH - ₴',
+				callback_data: 'UAH'
+			},
+			{
+				text: 'USD - $',
+				callback_data: 'USD'
+			}
+		],
+		[
+			{
+				text: '➡️',
+				callback_data: 'right_1'
+			}
+		]
+	],
+	inline_keyboard_2: [
+		[
+			{
+				text: 'EGP - ج.م.‏',
+				callback_data: 'EGP'
+			},
+			{
+				text: 'EUR - €',
+				callback_data: 'EUR'
+			},
+			{
+				text: 'GBP - £',
+				callback_data: 'GBP'
+			}
+		],
+		[
+			{
+				text: 'ISK - Ikr',
+				callback_data: 'ISK'
+			},
+			{
+				text: 'JPY - ¥',
+				callback_data: 'JPY'
+			},
+			{
+				text: 'KRW - ₩',
+				callback_data: 'KRW'
+			}
+		],
+		[
+			{
+				text: '⬅️',
+				callback_data: 'left_2'
+			},
+			{
+				text: '➡️',
+				callback_data: 'right_2'
+			}
+		]
+	],
+	inline_keyboard_3: [
+		[
+			{
+				text: 'KWD - د.ك.‏',
+				callback_data: 'KWD'
+			},
+			{
+				text: 'KZT - тңг.',
+				callback_data: 'KZT'
+			},
+			{
+				text: 'LBP - ل.ل.‏',
+				callback_data: 'LBP'
+			}
+		],
+		[
+			{
+				text: 'MYR - RM',
+				callback_data: 'MYR'
+			},
+			{
+				text: 'NOK - Nkr',
+				callback_data: 'NOK'
+			},
+			{
+				text: 'PLN - zł',
+				callback_data: 'PLN'
+			}
+		],
+		[
+			{
+				text: '⬅️',
+				callback_data: 'left_3'
+			},
+			{
+				text: '➡️',
+				callback_data: 'right_3'
+			}
+		]
+	],
+	inline_keyboard_4: [
+		[
+			{
+				text: 'AED - د.إ.‏',
+				callback_data: 'AED'
+			},
+			{
+				text: 'AUD - AU$',
+				callback_data: 'AUD'
+			},
+			{
+				text: 'CAD - CA$',
+				callback_data: 'CAD'
+			}
+		],
+		[
+			{
+				text: 'CHF - CHF',
+				callback_data: 'CHF'
+			},
+			{
+				text: 'CNY - CN¥',
+				callback_data: 'CNY'
+			},
+			{
+				text: 'DKK - Dkr',
+				callback_data: 'DKK'
+			}
+		],
+		[
+			{
+				text: '⬅️',
+				callback_data: 'left_4'
+			}
+		]
+	]
+};
